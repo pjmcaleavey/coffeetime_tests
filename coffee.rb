f053ad47ac5445d39b28d4d8b1d053e4c924fe97
Pat McAleavey <pmcaleavey8@gmail.com>
2016-03-02 09:05:03 -0500

    some progress, but not completed

---
+++ coffee.rb
@@ -0,0 +1,16 @@
+class Coffee
+
+  def initialize(coffee_type)
+    @coffee_type
+  end
+
+  def full?
+    false
+  end
+
+  def empty?
+    false
+  end
+
+
+end
--- human.rb
+++ human.rb
@@ -1,8 +1,27 @@
 class Human
 
   def initialize(name)
-    @name = name
+    @name
+    @alertness
   end
 
+  def alertness
+    @alertness = 0.0
+  end
 
+  def has_coffee?
+    false
+  end
+
+  def needs_coffee?
+    true
+  end
+
+  def buy(coffee_type)
+    
+  end
+
+  def drink!
+    if @alertness
+  end
 end
